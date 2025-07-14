5450f2d2b657f1af8c0dc11e0b88382b31be5464
Rachely-Yelloz <rsy262618@gmail.com>
2025-07-14 14:59:15 +0300

    add protectedlinkputmodel

---
+++ server/SafeShare.API/Models/ProtectedLinkPutModel.cs
@@ -0,0 +1,10 @@
+﻿namespace SafeShare.API.Models
+{
+    public class ProtectedLinkPutModel
+    {
+        public int FileId { get; set; }
+        public DateTime? ExpirationDate { get; set; }
+        public bool IsOneTimeUse { get; set; }
+        public int? DownloadLimit { get; set; }
+    }
+}
