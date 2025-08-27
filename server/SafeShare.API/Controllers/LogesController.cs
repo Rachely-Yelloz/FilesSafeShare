f66c3e1cbc94f77cf195bbb5e48408d60edd9778
Rachely-Yelloz <rsy262618@gmail.com>
2025-08-27 13:26:20 +0300

    add controller log

---
+++ server/SafeShare.API/Controllers/LogesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SafeShare.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LogesController : ControllerBase
+    {
+        private readonly ILogger<LogesController> _logger;
+
+        public LogesController(ILogger<LogesController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpPost("log")]
+        public IActionResult LogMessage()
+        {
+           _logger.LogInformation("This is a log message from LogesController.");
+            return Ok("Log message recorded.");
+        }
+    }
+}
